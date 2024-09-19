da307a3d1035e1a46340e678b4483dc47044a1ca
Chandan Rawat <131906959+its-cr7@users.noreply.github.com>
2024-09-19 23:24:45 +0530

    Time: 2 ms (44.94%), Space: 7.4 MB (17.96%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    uint reverseBits(uint n) {
+        int i = 0 , j = 31;
+        while(i < 16) {
+            if(((n>>i)&1) != ((n>>j)&1)) n ^= (1<<i) ^ (1<<j);
+
+            i++, j--;
+        }
+        return n;
+    }
+};
